826c4cef6ca5615c6b585f9dfcbbdd3bfce2b442
Claire0711 <71119574+Claire0711@users.noreply.github.com>
2021-03-20 14:57:13 +0100

    word verification OK

--- Projet-Java-S4-Serveur/src/mvc/modele/pendu/ImplPendu.java
+++ Projet-Java-S4-Serveur/src/mvc/modele/pendu/ImplPendu.java
@@ -1,7 +1,9 @@
 package mvc.modele.pendu;
 
 import java.rmi.RemoteException;
+
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.Random;
 
 @SuppressWarnings("serial")
@@ -9,10 +11,25 @@ public class ImplPendu extends UnicastRemoteObject implements InterfacePendu {
 
 	public ImplPendu() throws RemoteException {
 		super();
+		listeErreurs = new ArrayList<Integer>();
+		listeErreurs.add(0);
 	}
 
-	String dictionnaire[] = { "Claire", "Valentin", "Waian" };
-
+	String dictionnaire[] = {"claire", "waian", "valentin"};
+	int nbErreurs = 0; 
+	int idPartie = 0;
+	ArrayList<Integer> listeErreurs;
+	
+	@Override 
+	public String affichage(char[] motCache) {
+		String motAffichage = ""; 
+		for (int i =0; i<motCache.length;i++) {
+			motAffichage += motCache[i]; 
+			motAffichage += " "; 
+		}
+		return motAffichage; 
+	}
+	
 	@Override
 	public String generationMotAleatoire() throws RemoteException {
 		String mot = "";
@@ -21,14 +38,42 @@ public class ImplPendu extends UnicastRemoteObject implements InterfacePendu {
 		return mot;
 	}
 
+	
 	@Override
-	public char ecritLettres(char c) throws RemoteException {
-		return c;
+	public char[] ecritLettres(String mot, char lettre, char[] motCache) throws RemoteException {
+		boolean trouve = false; 
+		for (int i=0; i<mot.length(); i++) {
+			if (mot.charAt(i) == lettre) {
+				motCache[i] = mot.toUpperCase().charAt(i);  
+				trouve = true;
+			}
+		}
+		if (trouve == false) nbErreurs++; //listeErreurs.set(idPartie, listeErreurs.get(idPartie) + 1);
+		return motCache; 
+	}
+	
+	public int dessinerPendu() throws RemoteException {
+		return nbErreurs; 
 	}
 
+	/*
 	@Override
-	public void dessinerPendu() throws RemoteException {
-
+	public int dessinerPendu(int idPartie) throws RemoteException {
+		System.out.println(idPartie);
+		System.out.println(listeErreurs.get(idPartie));
+		return listeErreurs.get(idPartie); 
 	}
+	
+	@Override
+	public boolean partieTerminee (int idPartie) {
+		return false; 
+	}
+	
+	@Override
+    public int nouvellePartie() {
+        idPartie++; 
+        return idPartie; 
+    }
+	*/
 
 }
--- Projet-Java-S4-Serveur/src/mvc/modele/pendu/InterfacePendu.java
+++ Projet-Java-S4-Serveur/src/mvc/modele/pendu/InterfacePendu.java
@@ -1,13 +1,23 @@
 package mvc.modele.pendu;
 
 import java.rmi.Remote;
+
 import java.rmi.RemoteException;
 
 public interface InterfacePendu extends Remote {
-
-	public String generationMotAleatoire() throws RemoteException;
-
-	public char ecritLettres(char c) throws RemoteException;
-
-	public void dessinerPendu() throws RemoteException;
+	
+public String affichage(char[] c) throws RemoteException; 
+	
+	public String generationMotAleatoire() throws RemoteException; 
+	
+	public char[] ecritLettres (String mot, char c, char[] motDecompose) throws RemoteException; 
+	
+	public int dessinerPendu() throws RemoteException; 
+	
+	//public int dessinerPendu(int idPartie) throws RemoteException; 
+	
+	//public boolean partieTerminee(int idPartie) throws RemoteException; 
+	
+	//public int nouvellePartie() throws RemoteException; 
+	
 }
